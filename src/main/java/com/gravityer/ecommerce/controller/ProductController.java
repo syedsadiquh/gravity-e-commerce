df1d976a607104e3bf46fdeadbd0ad32747eb43e
syedsadiquh <syedsadiquh@gmail.com>
2025-09-09 11:51:45 +0530

    Added Paging and Sorting using PageRequest, Pageable and page

--- src/main/java/com/gravityer/ecommerce/controller/ProductController.java
+++ src/main/java/com/gravityer/ecommerce/controller/ProductController.java
@@ -4,6 +4,9 @@ import com.gravityer.ecommerce.models.Product;
 import com.gravityer.ecommerce.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -56,6 +59,27 @@ public class ProductController {
         var response = productService.deleteProduct(productId);
         if (response.isSuccess()) return ResponseEntity.ok(response);
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    // List Products (by paging and sorting)
+    @GetMapping("/listProducts")
+    public ResponseEntity<BaseResponse<Page<Product>>> listProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String sortBy,
+            @RequestParam(defaultValue = "true") boolean ascending
+    ) {
+        var sort = ascending ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        var pageable = PageRequest.of(page, size, sort);
+        var res = productService.listProducts(pageable);
+        if (res.isSuccess()) {
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        } else {
+            if (res.getMessage().contains("No Books Exists")) {
+                return new ResponseEntity<>(res, HttpStatus.NOT_FOUND);
+            }
+            return new ResponseEntity<>(res, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
