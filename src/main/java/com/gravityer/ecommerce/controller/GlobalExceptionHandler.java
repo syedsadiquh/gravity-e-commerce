67378244e512325e1d7cdac82b1560134b15f5ff
syedsadiquh <syedsadiquh@gmail.com>
2025-09-09 11:14:57 +0530

    Added Validation and GlobalExceptionHandler(ControllerAdvice)

---
+++ src/main/java/com/gravityer/ecommerce/controller/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.gravityer.ecommerce.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<BaseResponse<List<String>>> handleMissingAndInvalidArguments(MethodArgumentNotValidException exception) {
+        List<String> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .toList();
+        return new ResponseEntity<>(new BaseResponse<>(false, "Required Argument Missing", errors), HttpStatus.BAD_REQUEST);
+    }
+}
--- src/main/java/com/gravityer/ecommerce/models/Product.java
+++ src/main/java/com/gravityer/ecommerce/models/Product.java
@@ -1,6 +1,9 @@
 package com.gravityer.ecommerce.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -13,8 +16,17 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotEmpty
+    @Size(max = 100)
     private String productName;
+
+    @NotEmpty
     private String description;
+
+    @Min(0)
     private double price;
+
+    @Min(0)
     private int quantity;
 }
